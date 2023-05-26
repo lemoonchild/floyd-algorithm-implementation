26fdd34f4a6f160187ed83503e684d990bf5ce25
XavierLopez25 <lop22716@uvg.edu.gt>
2023-05-25 20:24:05 -0600

    Archivos de controlador, UI y tests agregados.

---
+++ src/JUnit/GraphManagerTest.java
@@ -0,0 +1,31 @@
+package JUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import Controller.ControladorGrafo;
+import org.junit.jupiter.api.Test;
+
+
+class GraphManagerTest {
+
+    @Test
+    void testShorterRoute() {
+
+    }
+
+    @Test
+    void testgetGraphCenter() {
+
+    }
+
+    @Test
+    void testBreakRoute() {
+
+    }
+
+    @Test
+    void testNewRoute() {
+
+    }
+
+}
